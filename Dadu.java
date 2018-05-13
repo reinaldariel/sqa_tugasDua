bad89d2c60d257709384e39c38ff72e705162e3c
reinaldariel <reinald.a.k@gmail.com>
2018-05-13 09:50:51 +0700

    tambah fungsi warna lur

--- Dadu.java
+++ Dadu.java
@@ -4,6 +4,7 @@ public static final int DEFAULT_SISI = 6;
 private static Random randGenerator = new Random();
 private final int jumSisi;
 private int hasil;
+private int warna;
 public Dadu() {
 this(DEFAULT_SISI);
 }
@@ -13,6 +14,9 @@ this.jumSisi = numSides;
 public int lempar() {
 hasil = randGenerator.nextInt(jumSisi) + 1;
 return hasil;
+public int getWarna(){
+warna = Color.rgb(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
+return warna;
 }
 public int getJumSisi() {
 return jumSisi;
@@ -22,6 +26,6 @@ return hasil;
 }
 @Override
 public String toString() {
-return "Jumlah sisi dadu: " + getJumSisi() + " hasil: " + getHasil();
+return "Jumlah sisi dadu: " + getJumSisi() + " hasil: " + getHasil() + " warna: "+ getWarna();
 }
 }
