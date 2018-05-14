0204c7d29291a17c80b724c0d599d7fb8ef41e5f
aloysiussaa <32889849+aloysiussaa@users.noreply.github.com>
2018-05-14 11:36:35 +0700

    Update Dadu.java

--- Dadu.java
+++ Dadu.java
@@ -1,35 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
 import java.util.Random;
-import java.awt.color;
+import java.awt.Color;
+/**
+ *
+ * @author Aloysius
+ */
 public final class Dadu {
-public static final int DEFAULT_SISI = 6;
-private static Random randGenerator = new Random();
-private final int jumSisi;
-private int hasil;
-private int warna;
-public Dadu() {
-this(DEFAULT_SISI);
-}
-public Dadu(int numSides) {
-this.jumSisi = numSides;
-}
-public int lempar() {
-hasil = randGenerator.nextInt(jumSisi) + 1;
-return hasil;
-public int getWarna(){
-warna = Color.rgb(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
-return warna;
-}
-public int getJumSisi() {
-return jumSisi;
-}
-public int getWarna(){
-warna = color.rgb(Rand.nextInt(255),Rand.nextInt(255),Rand.nextInt(255));
-}
-public int getHasil() {
-return hasil;
-}
-@Override
-public String toString() {
-return "Jumlah sisi dadu: " + getJumSisi() + " hasil: " + getHasil() + " warna: "+ getWarna();
-}
+    public static final int DEFAULT_SISI = 6;
+    private static Random randGenerator = new Random();
+    
+    private final int jumSisi;
+    private int hasil;
+    private int warna;
+    
+    public Dadu(){
+        this(DEFAULT_SISI);
+    }
+    public Dadu(int numSides){
+        this.jumSisi = numSides;
+    }
+    public int lempar(){
+        hasil = randGenerator.nextInt(jumSisi) + 1;
+        return hasil;
+    }
+    public int getJumSisi(){
+        return jumSisi;
+    }
+    public int getHasil(){
+        return hasil;
+    }
+    
+    public String getWarna(){
+        String[] colour = {"BLACK","WHITE","RED","PINK","YELLOW","GREEN"};
+        hasil = randGenerator.nextInt(colour.length);
+        return colour[hasil];
+    }
+    @Override
+    public String toString(){
+        return "Jumlah sisi dadu: "+ getJumSisi() + " hasil: " + getHasil() + " warna: " + getWarna(); 
+    }
+    
 }
